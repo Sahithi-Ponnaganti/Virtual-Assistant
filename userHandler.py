a30a7e7967cd191f4c7ba387383e70058c46dcd4
PONNAGANTI SAHITHI <95540146+Sahithi-Ponnaganti@users.noreply.github.com>
2023-05-26 13:57:21 +0530

    Add files via upload

---
+++ userHandler.py
@@ -0,0 +1,13 @@
+import pickle
+from pathlib import Path
+class UserData:
+	def __init__(self):
+		self.name = 'None'
+		self.gender = 'None'
+	def getName(self):
+		return self.name
+
+	def getGender(self):
+		return self.gender
+
+
